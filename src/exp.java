ab1082679939911ec313d6c329fb7f52e8afd354
karthik <karthiknr92@gmail.com>
2024-10-08 07:03:13 +0530

    ccommit all

---
+++ src/exp.java
@@ -0,0 +1,11 @@
+public class exp {
+    public static void main(String[] args) {
+        short s=-53;
+        int d=0;
+        int a =78;
+        int c= s+a;
+        //short d=((short)s+a);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
