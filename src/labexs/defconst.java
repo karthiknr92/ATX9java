dede0e8301d6fa0b8782414b7855cf490dbb8d3c
karthik <karthiknr92@gmail.com>
2024-10-09 18:32:50 +0530

    ccommit all

---
+++ src/labexs/defconst.java
@@ -0,0 +1,14 @@
+package labexs;
+public class defconst {
+    public static final String NAME = "Karthik";
+    public static final int AGE = 26;
+    public static final String LOCATION = "TAMILNADU";
+    public static final double PERFORMANCE_RATING = 92.25 ;
+
+    public static void main(String[] args) {
+        System.out.println("NAME :" +NAME);
+        System.out.println("AGE :" +AGE);
+        System.out.println("LOCATION :" +LOCATION);
+        System.out.println("PERFORMANCE RATING :" +PERFORMANCE_RATING);
+    }
+}
