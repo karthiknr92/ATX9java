a677cdc566a440fd75c1b1247cdfb2281be3c8e8
karthik <karthiknr92@gmail.com>
2024-10-09 07:00:53 +0530

    ccommit all

--- src/labexs/lab001.java
+++ src/labexs/lab001.java
@@ -2,6 +2,8 @@ package labexs;
 
 public class lab001 {
     public static void main(String[] args) {
-        System.out.printf("");
+        System.out.printf(" \n Karthik is awesome \n");
+        string name = "karthik";
+        System.out.println("your name is %s",name);
     }
 }
