7d937dade609e27365598c264ef02fb09f87c014
karthik <karthiknr92@gmail.com>
2024-10-09 08:01:49 +0530

    ccommit all

--- src/mulitpsop.java
+++ src/mulitpsop.java
@@ -7,25 +7,25 @@ public class mulitpsop {
         int a = sc.nextInt();
         // int a=6;
         System.out.println();
-        System.out.printf("%d*%d=%d",a,1,a*1);
+        System.out.printf("%d * %d = %d",a,1,a*1);
         System.out.println();
-        System.out.printf("%d*%d=%d",a,2,a*2);
+        System.out.printf("%d * %d = %d",a,2,a*2);
         System.out.println();
-        System.out.printf("%d*%d=%d",a,3,a*3);
+        System.out.printf("%d * %d = %d",a,3,a*3);
         System.out.println();
-        System.out.printf("%d*%d=%d",a,4,a*4);
+        System.out.printf("%d * %d = %d",a,4,a*4);
         System.out.println();
-        System.out.printf("%d*%d=%d",a,5,a*5);
+        System.out.printf("%d * %d = %d",a,5,a*5);
         System.out.println();
-        System.out.printf("%d*%d=%d",a,6,a*6);
+        System.out.printf("%d * %d = %d",a,6,a*6);
         System.out.println();
-        System.out.printf("%d*%d=%d",a,7,a*7);
+        System.out.printf("%d * %d = %d",a,7,a*7);
         System.out.println();
-        System.out.printf("%d*%d=%d",a,8,a*8);
+        System.out.printf("%d * %d = %d",a,8,a*8);
         System.out.println();
-        System.out.printf("%d*%d=%d",a,9,a*9);
+        System.out.printf("%d * %d = %d",a,9,a*9);
         System.out.println();
-        System.out.printf("%d*%d=%d",a,10,a*10);
+        System.out.printf("%d * %d = %d",a,10,a*10);
         System.out.println();
 
     }
