a41074896580592116e1c9a4c10fa28b7b5cf392
karthik <karthiknr92@gmail.com>
2024-10-16 06:27:47 +0530

    commit change

---
+++ src/Tasks/task_1610_1.java
@@ -0,0 +1,9 @@
+package Tasks;
+
+public class task_1610_1 {
+    public static void main(String[] args) {
+        int a=12;
+        System.out.println(++a + a++ + a++);
+        System.out.println(a);
+    }
+}
---
+++ src/Tasks/task_1610_t2.java
@@ -0,0 +1,8 @@
+package Tasks;
+
+public class task_1610_t2 {
+    public static void main(String[] args) {
+        int a=20;
+        System.out.println(--a + a++ + a--);
+        System.out.println(a);
+    }}
